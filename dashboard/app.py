e86e21d738e52cb8f93a888b68fbe18b8aac1749
Wai Phyo Ko <113586127+wpko@users.noreply.github.com>
2026-03-17 08:27:24 +0700

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from PIL import Image
 
-API_URL = "https://ai-image-classifier-web-app-fastapi-an8f.onrender.com"
+API_URL = "https://ai-image-classifier-web-app-fastapi-an8f.onrender.com/predict"
 
 st.title("AI Image Classifier")
 uploaded_file = st.file_uploader("Upload an image",type=['jpg','png','jpeg'])
